794fdf42ffbd82b7bbbfa2db143be34f4d562f9a
AToMiXhawK <githinmanesh@gmail.com>
2020-01-30 01:41:07 +0530

    Matrix multiplication using thread Intendation fixes in this commit

--- 04_matrix_thread_multiply.c
+++ 04_matrix_thread_multiply.c
@@ -10,7 +10,7 @@ int A[4][4], B[4][4], C[4][4];
 struct args
 {
 	int i;
-    int j;
+	int j;
 };
 
 void *multiply(void *a)
@@ -25,42 +25,42 @@ void *multiply(void *a)
 
 int main()
 {
-    srand(time(0));
-    r1 = rand()%3+2; c1 = rand()%3+1; r2 = c1; c2 = rand()%3+2;
-    pthread_t thread[r1*c2];
-    int tid = 0;
+	srand(time(0));
+	r1 = rand()%3+2; c1 = rand()%3+1; r2 = c1; c2 = rand()%3+2;
+	pthread_t thread[r1*c2];
+	int tid = 0;
 
-    for(int i=0; i<r1; i++)
-		for(int j=0; j<c1; j++)
-            A[i][j] = rand()%5;
+	for(int i=0; i<r1; i++)
+        for(int j=0; j<c1; j++)
+			A[i][j] = rand()%5;
 
-    for(int i=0; i<r2; i++)
-		for(int j=0; j<c2; j++)
-            B[i][j] = rand()%5;
+	for(int i=0; i<r2; i++)
+        for(int j=0; j<c2; j++)
+			B[i][j] = rand()%5;
 
-    printf("Array A:\n");
-    for(int i=0; i<r1; i++)
+	printf("Array A:\n");
+	for(int i=0; i<r1; i++)
 		{ for(int j=0; j<c1; j++) printf("%d\t",A[i][j]); printf("\n"); }
     
-    printf("\nArray B:\n");
-    for(int i=0;i<r2;i++)
+	printf("\nArray B:\n");
+	for(int i=0;i<r2;i++)
 		{ for(int j=0;j<c2;j++) printf("%d\t",B[i][j]); printf("\n"); }
 
-    for(int i=0; i<r1; i++)
+	for(int i=0; i<r1; i++)
 		for(int j=0;j<c2;j++)
-        {
-            struct args *p = (struct args*) malloc(sizeof(struct args));
-            p->i = i;
-            p->j = j;
-            pthread_create(&thread[tid++],NULL,multiply,(void *)p);
-        }
+		{
+			struct args *p = (struct args*) malloc(sizeof(struct args));
+			p->i = i;
+			p->j = j;
+			pthread_create(&thread[tid++],NULL,multiply,(void *)p);
+		}
     
-    for(int i=0; i<tid; i++)
+	for(int i=0; i<tid; i++)
 		pthread_join(thread[i],NULL);
 
-    printf("\nProduct:\n");
-    for(int i=0; i<r1; i++)
+	printf("\nProduct:\n");
+	for(int i=0; i<r1; i++)
 		{ for(int j=0; j<c2; j++) printf("%d\t",C[i][j]); printf("\n"); }
 
-    return 0;
+	return 0;
 }
